a02f5311a50624353a8e2e181d87d1608f0990b8
Mikolaj Kucharski <mikolaj@kucharski.name>
2019-05-24 06:34:06 +0000

    httpdate: move body of the code to a function
    - move everything to a function named main()
    - allow code to be imported, without executing it
    - drop microseconds from current time
    - use int instead of float to print time difference
    - update shebang line to use python2, hence drop obvious comment

--- httpdate.py
+++ httpdate.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python
-
-# python2.7
+#!/usr/bin/env python2
 
 # Copyright (c) 2017 Mikolaj Kucharski <mikolaj@kucharski.name>
 #
@@ -21,44 +19,48 @@ from httplib import HTTPConnection
 from subprocess import Popen, PIPE
 from sys import exit
 
-conn = HTTPConnection("www.google.com")
-conn.request("HEAD", "/")
-res = conn.getresponse()
+def main():
+	conn = HTTPConnection("www.google.com")
+	conn.request("HEAD", "/")
+	res = conn.getresponse()
 
-if res.status not in [200, 301, 302]:
-	raise Exception("Wrong HTTP status code")
+	if res.status not in [200, 301, 302]:
+		raise Exception("Wrong HTTP status code")
 
-for name, dh in res.getheaders():
-	if name != 'date':
-		continue
-	if not dh.endswith(' GMT'):
-		raise Exception('Date not in GMT timezone')
+	for name, dh in res.getheaders():
+		if name != 'date':
+			continue
+		if not dh.endswith(' GMT'):
+			raise Exception('Date not in GMT timezone')
 
-	print("Time from HTTP header is %s" % dh)
-	dt = datetime.strptime(dh, '%a, %d %b %Y %H:%M:%S %Z')
+		print("Time from HTTP header is %s" % dh)
+		dt = datetime.strptime(dh, '%a, %d %b %Y %H:%M:%S %Z')
 
-	# current time
-	ct = datetime.utcnow()
+		# current time
+		ct = datetime.utcnow().replace(microsecond=0)
 
-	print("Remote datetime is %s" % str(dt))
-	print("Local datatime is %s" % str(ct))
+		print("Remote datetime is %s" % str(dt))
+		print("Local datatime is %s" % str(ct))
 
-	delta = ct - dt
-	delta = abs(delta.total_seconds())
+		delta = ct - dt
+		delta = abs(delta.total_seconds())
 
-	print("Timedelta is %f seconds" % delta)
+		print("Timedelta is %d seconds" % delta)
 
-	if delta < 60 * 10:	# minutes
-		exit(0)
+		if delta < 60 * 10:	# minutes
+			exit(0)
 
-	nt = dt.strftime('%Y%m%d%H%M.%S')
-	cmd = ['date', '-u', '-s', nt]
-	print("Executing command %s" % ' '.join(cmd))
-	p = Popen(cmd, stdout=PIPE, stderr=PIPE)
-	stdoutdata, stderrdata = p.communicate()
+		nt = dt.strftime('%Y%m%d%H%M.%S')
+		cmd = ['date', '-u', '-s', nt]
+		print("Executing command %s" % ' '.join(cmd))
+		p = Popen(cmd, stdout=PIPE, stderr=PIPE)
+		stdoutdata, stderrdata = p.communicate()
 
-	print("stdout=%s" % stdoutdata.strip())
-	print("stderr=%s" % stderrdata.strip())
-	print("code=%d" % p.returncode)
+		print("stdout=%s" % stdoutdata.strip())
+		print("stderr=%s" % stderrdata.strip())
+		print("code=%d" % p.returncode)
 
-	exit(p.returncode)
+		exit(p.returncode)
+
+if __name__ == '__main__':
+    main()
