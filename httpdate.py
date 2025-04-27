2f5c93be1264914935439a45cc21491fea214d26
Mikolaj Kucharski <mikolaj@kucharski.name>
2025-04-27 10:43:16 +0000

    httpdate: use multiprocessing to retry fetching the time
    - use subprocess to spawn a function fetching the time
    - kill that subprocess if no results after a timeout
    - retry multiple times fetching and setting the system time
    - use exponential back-off between each try
    - bump timeouts in systemd unit file, as needed with the retries

--- httpdate.py
+++ httpdate.py
@@ -14,16 +14,23 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
+from __future__ import print_function
+
+import multiprocessing
+import time
+import sys
+
 from datetime import datetime
+
 try:
 	from httplib import HTTPConnection
 except ImportError:
 	from http.client import HTTPConnection
 from subprocess import Popen, PIPE
-from sys import exit
 
-def main():
-	conn = HTTPConnection("www.google.com")
+
+def http_time(ret):
+	conn = HTTPConnection("www.google.com", timeout=30)
 	conn.request("HEAD", "/")
 	res = conn.getresponse()
 
@@ -50,7 +57,8 @@ def main():
 		print("Timedelta is {} seconds".format(delta))
 
 		if delta < 60 * 10:	# minutes
-			exit(0)
+			ret.value = 0
+			return
 
 		nt = dt.strftime('%Y%m%d%H%M.%S')
 		cmd = ['date', '-u', '-s', nt]
@@ -62,10 +70,48 @@ def main():
 		print("stderr={}".format(stderrdata.strip()))
 		print("code={}".format(p.returncode))
 
-		exit(p.returncode)
+		ret.value = p.returncode
+
+
+def get_time():
+	manager = multiprocessing.Manager()
+	ret_val = manager.Value("i", 100)
+
+	proc = multiprocessing.Process(target=http_time, args=(ret_val,))
+	proc.start()
+
+	for _ in range(90):
+		if not proc.is_alive():
+			break
+		time.sleep(1)
+
+	if proc.is_alive():
+		print("Terminating {}".format(proc.name), file=sys.stderr)
+		proc.terminate()
+
+	proc.join()
+
+	return ret_val.value
+
+
+def main():
+	for num in range(10):
+		ret = get_time()
+		if ret == 0:
+			return 0
+		print("Failed unexpectedly with exit code {}".format(ret),
+			file=sys.stderr)
+		print("Sleeping for {} seconds...".format(2**num),
+			file=sys.stderr)
+		time.sleep(2**num)
+
+	print("Unable to finish operation successfully",
+		file=sys.stderr)
+	return 50
+
 
 if __name__ == '__main__':
 	try:
-		main()
+		sys.exit(main())
 	except KeyboardInterrupt:
 		pass
